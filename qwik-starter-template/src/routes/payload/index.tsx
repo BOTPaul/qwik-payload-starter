8f09726acf04b15d78c429f19ec062e24f05e799
Paul Johnson <3285385+BOTPaul@users.noreply.github.com>
2022-10-16 08:27:00 +0000

    feat: project setup for Qwik and Payload using dev containers

---
+++ qwik-starter-template/src/routes/payload/index.tsx
@@ -0,0 +1,47 @@
+import { component$, Resource } from "@builder.io/qwik";
+import { RequestHandler, useEndpoint } from "@builder.io/qwik-city";
+
+type UserData = {
+  id: string;
+  name: string;
+};
+
+type UsersData = UserData[];
+
+export const onGet: RequestHandler<UsersData> = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/users");
+    if (!response.ok)
+      return Promise.reject(
+        `Response wasn't OK: ${response.status} | ${response.statusText}`
+      );
+
+    const payload: { docs: UsersData } = await response.json();
+
+    return payload.docs;
+  } catch (err) {
+    console.error(err);
+    return Promise.reject(`Error with fetch. See logs.`);
+  }
+};
+
+export default component$(() => {
+  const usersData = useEndpoint<UsersData>();
+
+  return (
+    <Resource
+      value={usersData}
+      onPending={() => <div>Loading...</div>}
+      onRejected={() => <div>Error</div>}
+      onResolved={(users) => (
+        <ul>
+          {users.map((user) => (
+            <li>
+              {user.id} | {user.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    />
+  );
+});
